47042b7888f7dd4841958e97a539666a6b6fdd58
pc <pcwow789@gmail.com>
2014-08-25 22:00:32 +0800

    v0.01

--- app.js
+++ app.js
@@ -2,4 +2,4 @@ var connect = require("connect");
 var serve_static = require("serve-static");
 var app = connect();
 app.use(serve_static(__dirname + "/public"));
-app.listen(3000);
+app.listen(process.env.VCAP_APP_PORT || 3000);
